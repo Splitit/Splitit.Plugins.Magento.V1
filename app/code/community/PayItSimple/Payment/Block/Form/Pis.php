5d99035b422b2cb69651b6a5673b7d4458bc93d4
manniwaliyan <Ujjwal@mobikasa.com>
2017-01-20 18:40:41 +0530

    fix changes required in excel sheet

--- app/code/community/PayItSimple/Payment/Block/Form/Pis.php
+++ app/code/community/PayItSimple/Payment/Block/Form/Pis.php
@@ -28,6 +28,76 @@ class PayItSimple_Payment_Block_Form_Pis extends Mage_Payment_Block_Form_Cc
         $selectInstallmentSetup = Mage::getStoreConfig('payment/pis_cc/select_installment_setup');
         $installmentsInDropdown = [];
         $options = Mage::getModel('pis_payment/source_installments')->toOptionArray();
+        
+        $depandOnCart = 0;
+        // $selectInstallmentSetup == "" for checking when merchant first time upgrade extension that time $selectInstallmentSetup will be empty
+        if($selectInstallmentSetup == "" || $selectInstallmentSetup == "fixed"){ // Select Fixed installment setup
+            
+            $fixedInstallments = Mage::getStoreConfig('payment/pis_cc/available_installments');
+            foreach (explode(',', $fixedInstallments) as $n) {
+                
+                if((array_key_exists($n, $options))){
+                    $installments[$n] = $n.' Installments of '. Mage::app()->getLocale()->currency(Mage::app()->getStore()->getCurrentCurrencyCode())->getSymbol().round($totalAmount/$n,2); 
+                    $installmentsInDropdown[$n] = round($totalAmount/$n,2); 
+                }
+            }
+            
+        }else{ // Select Depanding on cart installment setup
+            $depandOnCart = 1;  
+            $depandingOnCartInstallments = Mage::getStoreConfig('payment/pis_cc/depanding_on_cart_total_values');
+            $depandingOnCartInstallmentsArr = json_decode($depandingOnCartInstallments);
+            $dataAsPerCurrency = [];
+            foreach($depandingOnCartInstallmentsArr as $data){
+                $dataAsPerCurrency[$data->doctv->currency][] = $data->doctv;
+            }
+            $currentCurrencyCode = Mage::app()->getStore()->getCurrentCurrencyCode();
+            if(count($dataAsPerCurrency) && isset($dataAsPerCurrency[$currentCurrencyCode])){
+                
+                foreach($dataAsPerCurrency[$currentCurrencyCode] as $data){
+                    if($totalAmount >= $data->from && !empty($data->to) && $totalAmount <= $data->to){
+                        foreach (explode(',', $data->installments) as $n) {
+
+                            if((array_key_exists($n, $options))){
+                                $installments[$n] = $n.' Installments of '. Mage::app()->getLocale()->currency(Mage::app()->getStore()->getCurrentCurrencyCode())->getSymbol().round($totalAmount/$n,2);    
+                                $installmentsInDropdown[$n] = round($totalAmount/$n,2);
+                                                        
+                            }
+                        }
+                        break;
+                    }else if($totalAmount >= $data->from && empty($data->to)){
+                        foreach (explode(',', $data->installments) as $n) {
+
+                            if((array_key_exists($n, $options))){
+                                $installments[$n] = $n.' Installments of '. Mage::app()->getLocale()->currency(Mage::app()->getStore()->getCurrentCurrencyCode())->getSymbol().round($totalAmount/$n,2);  
+                                $installmentsInDropdown[$n] = round($totalAmount/$n,2); 
+                                                         
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+        } 
+
+
+
+        if(count($installments) == 0){
+            $installments[] = "Installments are not available.";
+        }
+        // set how much installments to be show in checkout page dropdown
+        Mage::getSingleton('core/session')->setInstallmentsInDropdown($installmentsInDropdown);
+        
+        return $installments;
+    }    
+
+public function getAvailableInstallments_copy()
+    {
+        $method = $this->getMethod();
+        $installments = array();
+        $totalAmount = Mage::getSingleton('checkout/session')->getQuote()->getGrandTotal();
+        $selectInstallmentSetup = Mage::getStoreConfig('payment/pis_cc/select_installment_setup');
+        $installmentsInDropdown = [];
+        $options = Mage::getModel('pis_payment/source_installments')->toOptionArray();
         $below100 = 0;
         $depandOnCart = 0;
         if($totalAmount < 100){
@@ -37,7 +107,7 @@ class PayItSimple_Payment_Block_Form_Pis extends Mage_Payment_Block_Form_Cc
             
             if($selectInstallmentSetup == "fixed"){ // Select Fixed installment setup
             
-                $fixedInstallments = Mage::getStoreConfig('payment/pis_cc/fixed_installment');
+                $fixedInstallments = Mage::getStoreConfig('payment/pis_cc/available_installments');
                 foreach (explode(',', $fixedInstallments) as $n) {
                     
                     if((array_key_exists($n, $options))){
@@ -92,8 +162,7 @@ class PayItSimple_Payment_Block_Form_Pis extends Mage_Payment_Block_Form_Cc
         Mage::getSingleton('core/session')->setInstallmentsInDropdown($installmentsInDropdown);
         
         return $installments;
-    }    
-
+    } 
     public function getMethodLabelAfterHtml(){
         $markFaq = Mage::getConfig()->getBlockClassName('core/template');
         $markFaq = new $markFaq;
--- app/code/community/PayItSimple/Payment/Model/Observer.php
+++ app/code/community/PayItSimple/Payment/Model/Observer.php
@@ -17,4 +17,86 @@ class PayItSimple_Payment_Model_Observer
             $_block->setTemplate('payitsimple/splitprice.phtml');
         }
     }
+
+    public function paymentMethodIsActive(Varien_Event_Observer $observer) {
+        
+        $event           = $observer->getEvent();//print_r($event->getData());die("---sdf");
+        $method          = $event->getMethodInstance();
+        $result          = $event->getResult();
+        $currencyCode    = Mage::app()->getStore()->getCurrentCurrencyCode();
+
+
+            if($method->getCode() == "pis_cc"){
+                $result->isAvailable = $this->checkAvailableInstallments();
+            }/*else{
+                $result->isAvailable = false;
+            }*/
+
+    }
+
+    private function checkAvailableInstallments(){
+        $installments = array();
+        $totalAmount = Mage::getSingleton('checkout/session')->getQuote()->getGrandTotal();
+        $selectInstallmentSetup = Mage::getStoreConfig('payment/pis_cc/select_installment_setup');
+        $installmentsInDropdown = [];
+        $options = Mage::getModel('pis_payment/source_installments')->toOptionArray();
+        
+        $depandOnCart = 0;
+        // $selectInstallmentSetup == "" for checking when merchant first time upgrade extension that time $selectInstallmentSetup will be empty
+        if($selectInstallmentSetup == "" || $selectInstallmentSetup == "fixed"){ // Select Fixed installment setup
+            
+            $fixedInstallments = Mage::getStoreConfig('payment/pis_cc/available_installments');
+            foreach (explode(',', $fixedInstallments) as $n) {
+                
+                if((array_key_exists($n, $options))){
+                    $installments[$n] = $n.' Installments of '. Mage::app()->getLocale()->currency(Mage::app()->getStore()->getCurrentCurrencyCode())->getSymbol().round($totalAmount/$n,2); 
+                    $installmentsInDropdown[$n] = round($totalAmount/$n,2); 
+                }
+            }
+            
+        }else{ // Select Depanding on cart installment setup
+            $depandOnCart = 1;  
+            $depandingOnCartInstallments = Mage::getStoreConfig('payment/pis_cc/depanding_on_cart_total_values');
+            $depandingOnCartInstallmentsArr = json_decode($depandingOnCartInstallments);
+            $dataAsPerCurrency = [];
+            foreach($depandingOnCartInstallmentsArr as $data){
+                $dataAsPerCurrency[$data->doctv->currency][] = $data->doctv;
+            }
+            $currentCurrencyCode = Mage::app()->getStore()->getCurrentCurrencyCode();
+            if(count($dataAsPerCurrency) && isset($dataAsPerCurrency[$currentCurrencyCode])){
+                
+                foreach($dataAsPerCurrency[$currentCurrencyCode] as $data){
+                    if($totalAmount >= $data->from && !empty($data->to) && $totalAmount <= $data->to){
+                        foreach (explode(',', $data->installments) as $n) {
+
+                            if((array_key_exists($n, $options))){
+                                $installments[$n] = $n.' Installments of '. Mage::app()->getLocale()->currency(Mage::app()->getStore()->getCurrentCurrencyCode())->getSymbol().round($totalAmount/$n,2);    
+                                $installmentsInDropdown[$n] = round($totalAmount/$n,2);
+                                                        
+                            }
+                        }
+                        break;
+                    }else if($totalAmount >= $data->from && empty($data->to)){
+                        foreach (explode(',', $data->installments) as $n) {
+
+                            if((array_key_exists($n, $options))){
+                                $installments[$n] = $n.' Installments of '. Mage::app()->getLocale()->currency(Mage::app()->getStore()->getCurrentCurrencyCode())->getSymbol().round($totalAmount/$n,2);  
+                                $installmentsInDropdown[$n] = round($totalAmount/$n,2); 
+                                                         
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+        } 
+
+
+
+        if(count($installments) == 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
 }
