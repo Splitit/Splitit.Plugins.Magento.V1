4393640b1115ff78ec02d0f445e45f4d47cf36c4
manniwaliyan <manish@mobikasa.com>
2019-05-08 15:40:23 +0530

    remoe curl_setopt(, CURLOPT_POST, 1) from getApprovalUrlResponse

--- app/code/community/PayItSimple/Payment/Model/Api.php
+++ app/code/community/PayItSimple/Payment/Model/Api.php
@@ -259,7 +259,7 @@ class PayItSimple_Payment_Model_Api extends Mage_Core_Model_Abstract
         //$jsonData = json_encode($params);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");  
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POST, 1);
+        // curl_setopt($ch, CURLOPT_POST, 1);
         //curl_setopt($ch, CURLOPT_POSTFIELDS,$jsonData);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); 
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
